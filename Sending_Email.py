0cf18b9a74911a9ecc8bdc89256d61ea6401c316
SuWanKim-code <73262265+SuWanKim-code@users.noreply.github.com>
2020-10-22 14:44:44 +0900

    Update Sending_Email.py

--- Sending_Email.py
+++ Sending_Email.py
@@ -1,14 +1,39 @@
 import smtplib
 from email.mime.text import MIMEText
-
+from email.mime.image import MIMEImage
  
-def sendMail(my_email, recipient_email, msg):
+def sendMail(my_email, recipient_email, msg, feed_num):
+  """
+  완성된 기사와 차트를 이메일로 보내는 함수
+
+  params:
+    my_email : 내 email 주소
+    recipient_email : 받을 상대방의 email 주소
+    msg : 기사 제목 및 내용
+    feed_num: 공시의 고유번호
+  """
     smtp = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-    smtp.login(my_email, 'cyuidhhqqepdqhmo') 
+    smtp.login(my_email, 'cyuidhhqqepdqhmo') #나의 앱비밀번호
+    # 이메일 TEXT
     msg = MIMEText(msg) # 기사내용, 제목 등등 
+
+    #이메일 IMAGE
+    img1_path = 'db/chart/' + feed_num + '-chart'
+    img2_path =  'db/chart/' + feed_num + '-bar'
+    pngfiles = [img1_path, img2_path]
+    filename = ['Chart', 'Bar']
+    for i in range(0,2):
+      fp = open(pngfiles[i], 'rb')
+      img = MIMEImage(fp.read())
+      fp.close()
+      # 첨부한 파일의 파일이름을 입력합니다. (이 구문이 없으면 noname으로 발송됩니다.)
+      img.add_header(filename[i] , 'attachment', filename=file)
+      msg.attach(img)
+
+    # 이메일 제목
     msg['Subject'] = '이메일 제목'
+
+    #Sending Email
     smtp.sendmail(my_email, recipient_email, msg.as_string())
     smtp.quit()
-
-sendMail('tndhks3837@gmail.com', 'swan3837@naver.com', '실습 테스트 메일입니다') 
 
