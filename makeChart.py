e3e4aa9b19183ac0e4504c98df966f4798d3cab2
baobabtr33 <59995362+baobabtr33@users.noreply.github.com>
2020-10-22 13:09:44 +0900

    Update makeChart.py

--- makeChart.py
+++ makeChart.py
@@ -27,18 +27,48 @@ def draw_stock_chart(df, corpname, filing_num):
     plt.title(corpname + ", 최근 3개월",fontproperties=fontprop, loc = "right")
     plt.grid(True) # 그리드
     plt.xticks(series_df.index[::7],series_df.index[::7], rotation=45, ha="right")  #ha 로 tick에 맞춘다. / 7일에 한 번 tick label
-    file_save = 'db/chart/'+ filing_num
+    file_save = 'db/chart/'+ filing_num + '-chart'
     plt.savefig(file_save, bbox_inches='tight')
 
     return file_save
 
+# TODO: Bar 옆에 label 붙이기
+def comparison_chart(chart_title, filing_num, comp1_name, comp2_name, comp1_num, comp2_num):
+    """
+    비교 차트 생성, 비교 차트 주소 반환.
 
+    params:
+        chart_title : 차트에 들어갈 title
+        filing_num : 공시 보고서 고유 번호
+        comp1_name : 라벨1 이름
+        comp2_name : 라벨2 이름
+        comp1_num : 라벨1 값
+        comp2_num : 라벨2 값
 
+    return:
+        file_save : 저장한 위치, 파일명을 반환
+    """
+    comp1_num = int(re.search(r'\d+', comp1_num).group()) # to int
+    comp2_num = int(re.search(r'\d+', comp2_num).group())
 
+    comp1_num = comp1_num / math.pow(10,8) # 억단위로
+    comp2_num = comp2_num / math.pow(10,8)
 
-## make bottom chart
-## f(X)compare chart horizontal
+    y_axis = [comp1_name, comp2_name]
+    x_axis = [float("{:.2f}".format(comp1_num)),float("{:.2f}".format(comp2_num))]
+    y_pos = np.arange(len(y_axis))
 
+    # Create horizontal bars
+    bars = plt.barh(y_pos, x_axis, height = 0.5)
+    font_path = 'data/kor_font/NanumBarunGothic.ttf'
+    fontprop = fm.FontProperties(fname=font_path, size=10)
 
-## make chart
-# def create_horizontal_compare(계약)
+    plt.title('단위 : 억 원', loc='right', fontsize = 10, fontproperties = fontprop)
+    plt.title(chart_title, fontproperties = fontprop, fontsize = 15)
+    # Create names on the y-axis
+    plt.yticks(y_pos, y_axis)
+
+    file_save = 'db/chart/'+ filing_num + '-bar'
+    plt.savefig(file_save, bbox_inches='tight')
+
+    return file_save
