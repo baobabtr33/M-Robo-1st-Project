fbb6a0f23196bb285e7cb521b19fccd5710c6d1b
baobabtr33 <59995362+baobabtr33@users.noreply.github.com>
2020-10-26 16:38:55 +0900

    로그 생성 방법 finalize
    Signed-off-by: JUNGHWAN KIM <stevekim0131@naver.com>

--- main.py
+++ main.py
@@ -144,29 +144,7 @@ def test_ver(my_driver_path):
 
 
 if __name__ == "__main__":
-
     # 로그 생성
-    """
-    global logger
-    logger = logging.getLogger(__name__)
-
-    # 로그의 출력 기준 설정
-    logger.setLevel(logging.INFO)
-
-    # log 출력 형식
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-    # log 출력
-    stream_handler = logging.StreamHandler()
-    stream_handler.setFormatter(formatter)
-    logger.addHandler(stream_handler)
-
-    # log를 파일에 출력
-    file_handler = logging.FileHandler('supply_contract.log')
-    file_handler.setFormatter(formatter)
-    logger.addHandler(file_handler)
-    """
-
     logging.config.fileConfig('logging.conf')
     logger = logging.getLogger(__name__)
 
