4fa8a9b5217d3b44798d9a740853e9a3b3bb5b87
JUNGHWAN KIM <stevekim0131@naver.com>
2020-10-28 10:49:40 +0900

    example
    Signed-off-by: JUNGHWAN KIM <stevekim0131@naver.com>

--- mLog.py
+++ mLog.py
@@ -7,14 +7,14 @@ def status_checker(name, status):
     if status != 200:
         status_category = str(status)[0]
         if status_category == '3':
-            logger.error("DART RSS: HTTPS status - {} - redirection".format(status))
+            logger.warning("DART RSS: HTTPS status - {} - redirection".format(status))
             # continue
         elif status_category == '4':
-            logger.warning("DART RSS: HTTPS status - {} - client error".format(status))
+            logger.critical("DART RSS: HTTPS status - {} - client error".format(status))
             exit()
         elif status_category == '5':
-            logger.warning("DART RSS: HTTPS status - {} - server error".format(status))
-
+            logger.critical("DART RSS: HTTPS status - {} - server error".format(status))
+            exit()
         else:
             logger.INFO("Something is wrong with RSS request. Status Code: {}".format(status))
             # continue
